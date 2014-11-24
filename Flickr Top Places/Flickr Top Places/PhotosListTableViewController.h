37b6ed6c8862c4c915049743e36fe884d9c09121
Parmesh Bayappu <PBayappu@wilburellis.com>
2014-11-24 14:10:21 -0800

    The basic photo (descriptions) table view completed. Not yet hooked to the skeleton project.

---
+++ Flickr Top Places/Flickr Top Places/PhotosListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  PhotosListTableViewController.h
+//  Flickr Top Places
+//
+//  Created by Parmesh Bayappu on 11/21/14.
+//  Copyright (c) 2014 Parmesh Bayappu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosListTableViewController : UITableViewController
+
+@property (nonatomic) NSDictionary *placeOfPhotos;
+
+@end
