396074331552443fb424c617960d444ae9e6e096
Parmesh Bayappu <PBayappu@wilburellis.com>
2014-12-28 23:47:36 -0800

    Base version for Top Regions  - Based of Top Places.

---
+++ Top Regions/Top Regions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Top Regions
+//
+//  Created by Parmesh Bayappu on 12/15/14.
+//  Copyright (c) 2014 Parmesh Bayappu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Top Regions/Top Regions/Views/PhotoViewController.h
@@ -0,0 +1,15 @@
+//
+//  PhotoViewController.h
+//  Flickr Top Places
+//
+//  Created by Parmesh Bayappu on 11/25/14.
+//  Copyright (c) 2014 Parmesh Bayappu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UIViewController
+
+@property (nonatomic) NSDictionary *photoMetaData;
+
+@end
